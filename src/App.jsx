e4a5a9257b384cc05872bd7a33977f1536af97e1
Santhosh <59262371+santhoshfsd@users.noreply.github.com>
2021-03-02 12:51:00 +0530

    added routes (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './page/homepage/homepage.component'
+
+const hats = () => {
+  return <div>Hats page</div>
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/hats" component={hats}></Route>
+
     </div>
   );
 }
