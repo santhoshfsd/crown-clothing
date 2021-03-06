1bba215bdf3e62f8f5c4f9c011781932d76dd39e
santhoshvfsd <santhoshvfsd@outlook.com>
2021-03-06 05:41:54 +0530

    redirect user

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 
@@ -17,6 +17,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+
     const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
       console.log(user);
@@ -30,7 +31,7 @@ class App extends React.Component {
           })
         })
       } else {
-        setCurrentUser({ user })
+        setCurrentUser(user)
       }
     });
   }
@@ -46,15 +47,30 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser !== null
+                ? (
+                  <Redirect to='/' />
+                ) : (
+                  <SignInAndSignUpPage />
+                )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,16 +1,19 @@
+import { UserActionTypes } from './user.types';
 
-const IntialState = {
+const INITIAL_STATE = {
     currentUser: null
-}
+};
 
-const userReducer = (state = IntialState, action) => {
+const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
-            return { ...state, currentUser: action.payload }
-
+        case UserActionTypes.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: action.payload
+            };
         default:
-            return state
+            return state;
     }
-}
+};
 
 export default userReducer;
