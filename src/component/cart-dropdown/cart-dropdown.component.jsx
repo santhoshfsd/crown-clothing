f3fe8065ca9f9d5e611a31308e257c667550072d
santhoshvfsd <santhoshvfsd@outlook.com>
2021-03-07 19:12:16 +0530

    hide checkout dropdown

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -8,8 +8,9 @@ import './cart-dropdown.styles.scss';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect'
 import { withRouter } from 'react-router';
+import { toggleCartHidden } from './../../redux/cart/cart.action'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {cartItems.length ? cartItems.map(cartItem => (
@@ -18,7 +19,10 @@ const CartDropdown = ({ cartItems, history }) => (
                 <span className="empty-message">Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            dispatch(toggleCartHidden())
+            history.push('/checkout')
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
