69a4aea8073cad71aab0e8141239d2fbf8c69efe
santhoshvfsd <santhoshvfsd@outlook.com>
2021-03-07 06:01:07 +0530

    adding styling cart item

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import './cart-dropdown.styles.scss'
-import CartItem from './../cart-item/cart-item.component'
-import CustomButton from './../custom-button/custom-button.component'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 
-const CartDropdown = ({ cartItems }) => {
-    return <div className="cart-dropdown">
-        <div className="cart-items">
-            {cartItems.map(item => {
-                return <CartItem id={item.id} item={item} />
-            })}
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = ({ cartItems }) => (
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
         </div>
-        <CustomButton type="button"> Checkout</CustomButton>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-}
+);
 
-const mapStateToProps = ({ cart: cartItems }) => ({
-    cartItems: cartItems
-})
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/component/cart-item/cart-item.component.jsx
+++ src/component/cart-item/cart-item.component.jsx
@@ -6,7 +6,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
         <img src={imageUrl} alt="item"></img>
         <div className="item-details">
             <span className="name">{name}</span>
-            <span className="price">{price}</span>
+            <span className="price"> {quantity} x ${price}</span>
         </div>
 
     </div>
