bbe4b24c05910fe467c13b7112ba382f1bccc09a
santhoshvfsd <santhoshvfsd@outlook.com>
2021-03-07 11:48:01 +0530

    user redux selector

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ import Header from './component/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.action'
-
+import { selectCurrentUser } from './redux/user/user.selector'
+import { createStructuredSelector } from 'reselect'
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
@@ -65,8 +66,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
