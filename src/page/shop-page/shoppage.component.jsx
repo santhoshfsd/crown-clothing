1c5b3fb0dca36758715435166e85fdbc7a14c05f
santhoshvfsd <santhoshvfsd@outlook.com>
2021-03-09 06:55:28 +0530

    shop reducer

--- src/page/shop-page/shoppage.component.jsx
+++ src/page/shop-page/shoppage.component.jsx
@@ -1,30 +1,26 @@
 
 import React from 'react'
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom'
-import SHOP_DATA from './shop.data'
 
 import CollectionPreview from "./../../component/collection-preview/collection-preview.component";
 
-class ShopPage extends React.Component {
+import { createStructuredSelector } from "reselect"
+import { selectCollections } from './../../redux/shop/shop.selector'
 
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => {
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                {collections.map(({ id, ...previewItems }) => {
-                    return <CollectionPreview key={id} {...previewItems} />
-                })}
-            </div>
-        )
-    }
+    return (
+        <div className="shop-page">
+            {collections.map(({ id, ...previewItems }) => {
+                return <CollectionPreview key={id} {...previewItems} />
+            })}
+        </div>
+    )
 }
 
-export default withRouter(ShopPage)
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(withRouter(ShopPage))
--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -8,6 +8,8 @@ import cartReducer from './cart/cart.reducer';
 
 import directoryReducer from './directory/directory.reducer';
 
+import shopReducer from './shop/shop.reducer';
+
 const persistConfig = {
     key: 'root',
     storage,
@@ -17,7 +19,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-    directory: directoryReducer
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer)
